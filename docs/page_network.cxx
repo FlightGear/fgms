49a22489962bbb3d0116e0034a20d97804ef280a
Peter Morgan <pete@daffodil.uk.com>
2013-03-29 19:04:53 +0000

    tidy pages

--- docs/page_network.cxx
+++ docs/page_network.cxx
@@ -3,9 +3,9 @@
  * 
  * The \ref FlightGear multiplayer network of around twenty and counting (circa 2013) server. 
  * * These servers are public internet facing.
- * * Public servers are meant to run permanently on a host, and let players connect to them.
+ * * Public servers are meant to run permanently on a host, and let players connect to them for free.
  * 
  * 
  * \section hosting_info Hosting and Mp Server
- * 
+ * You are more then welcome
  */
