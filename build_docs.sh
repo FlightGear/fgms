34dc3dfd7770be46ce96e1c9d10c47e5dca35e58
Peter Morgan <pete@daffodil.uk.com>
2013-04-02 23:08:43 +0100

    Cmake detects version

--- build_docs.sh
+++ build_docs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Script to compile the docs, requires doxygen 1.8+
+## see http://stackoverflow.com/questions/11032280/specify-doxygen-parameters-through-command-line
 
 ## TODO Need to override the OUTPUT_DIRECTORY
 ##      its currently set to build_docs/
