950177b514852669fdbc22b8d6fe0a07cdc811ff
Peter Morgan <pete@daffodil.uk.com>
2013-04-13 03:39:40 +0100

    Added screenshots and 404.html page (this is configued server side

--- docs/pages/page_main.cpp
+++ docs/pages/page_main.cpp
@@ -1,9 +1,18 @@
 
 /*! \mainpage Home
  *
- * The <b>F</b>light<b>G</b>ear <b>M</b>ulti<b>P</b>layer Server ( \ref fgms ) 
- * - is a standalone network server that powers the \ref FlightGear \ref mp_network 
+ * The <b>F</b>light<b>G</b>ear <b>M</b>ulti<b>P</b>layer Server (\ref fgms) 
+ * is a standalone network server that powers the \ref FlightGear \ref mp_network.
+ * It allows flying with other pilots over a network inside \ref fgfs.
  * 
+ * <table class="screenshots">
+ * <tr>
+ * <td><img src="MP_01.jpg"  class="gallery" height="100" alt="Credit: robitabu"></td>
+ * <td><img src="flightgear-9.jpg"  class="gallery" height="100" alt="Credit: http://flightgear.en.softonic.com/images-videos"></td>
+ * <td><img src="Screenshot_7.jpg"  class="gallery" height="100" alt="Credit: hellcat http://www.flightgear.org/forums/viewtopic.php?f=19&t=9022&start=30"></td>
+ * <td><img src="fg-mp-1.png"  class="gallery" height="100" alt="Credit: Pigeon  http://pigeond.net/photos/flightgear/multiplay/14"></td>
+ * </tr>
+ * </table>
  * \note 
  *     <b>NOTAM: Attention all Pilots</b><br>
  *     This site is to do with the server project itself, its development, installation, code, etc.<br>
@@ -102,8 +111,10 @@
  * \page fgms_init_d init_d.fgms-skel
  *  \include ./contrib/etc/init_d.fgms-skel
  * 
-  * \page supervisor_conf supervisor.fgms.conf
+ * \page supervisor_conf supervisor.fgms.conf
  *  \include ./contrib/etc/supervisor.fgms.conf
+ * 
+ * 
  * 
  */
 
